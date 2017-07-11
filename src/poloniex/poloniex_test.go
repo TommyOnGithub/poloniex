848455fc96106422d103aade25b0024707ab4706
Thomas Barry <tbarry3@mail.depaul.edu>
2017-07-10 23:53:45 -0500

    messing around with tests

--- src/poloniex/poloniex_test.go
+++ src/poloniex/poloniex_test.go
@@ -3,6 +3,7 @@ package poloniex
 import (
 	"testing"
 	"regexp"
+	"strings"
 )
 
 func TextReturnTicker(t *testing.T) {
@@ -43,15 +44,38 @@ func TestReturn24hVolume(t *testing.T) {
 }
 
 func TestReturnOrderBook(t *testing.T) {
-	wantRegexp := regexp.MustCompile("\\{\"asks\":\\[(\\[\"[0-9]+\\.[0-9]{8}\",[0-9]+\\.?[0-9\\We]{0,8}\\]," +
-		")+\\[\"[0-9]+\\.[0-9]{8}\",[0-9]+\\.[0-9]{8}\\]\\],\"isFrozen\":\"0\",\"seq\":[0-9]{9}\\}")
+	var matched bool
+	var err 	error
+	wantRegexp := regexp.MustCompile("\\{\"asks\":\\[\\[\"[0-9]+\\.[0-9]{8}\",[0-9]+\\.?[0-9\\We]{0,8}\\]" +
+		",)+\\[\"[0-9]+\\.[0-9]{8}\",[0-9]+\\.[0-9]{8}\\]\\],\"bids\":\\[([\"[0-9]+.[0-9]{8}\",[0-9]+.?[0-9]{0,8}\\]" +
+		",)+[\"[0-9]+.[0-9]{8}\",[0-9]+.?[0-9]{0,8}\\]\\],\"isFrozen\":\"0\",\"seq\":[0-9]{9}\\}")
 	got, err := ReturnOrderBook("USDT_ETH")
 	if err != nil {
 		t.Error(err)
 	} else {
-		matched, _ := regexp.MatchString(wantRegexp.String(), got)
-		if !matched {
-			t.Fatalf("HTTPS response does not match expected; got: %s\n", got)
+		matched = wantRegexp.MatchString( got)
+	}
+	if err != nil {
+		t.Errorf("HTTPS response does not match expected; got: %s\n\nError Output:\n\n%v\n", got, err)
+	} else if !matched {
+		t.Fatalf("HTTPS response does not match expected; got: %s\n\nErrorOutput:\n\n%v\n", got, err)
+	}
+	var subRegex string
+	Outer:
+	for _, part := range strings.Split(got, ",") {
+		for _, subRegex = range strings.Split(wantRegexp.String(), ",") {
+			tmp := strings.Replace(subRegex, "(", "", 0)
+			subRegex = strings.Replace(tmp, ")", ",", 0)
+			comp := regexp.MustCompile(subRegex)
+			matched = comp.MatchString(part)
+			if err != nil {
+				t.Errorf("Error matching string: %v\n", err)
+			} else if !matched {
+				t.Logf("Could not match; %s and %s\n", subRegex, part)
+			} else {
+				break Outer
+			}
 		}
+		t.Fatalf("Mismatch section; want: %v, got: %s\n", strings.Split(wantRegexp.String(), ","), part)
 	}
 }
